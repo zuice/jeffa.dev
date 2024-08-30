b58c56be3addbddaa19f7fd95dcdf642294530e5
zuice <jeff.allen127@gmail.com>
2024-08-30 12:36:11 -0400

    Adjust styles to look a bit better on mobile, add color to header

--- packages/web/src/layout/Landing.tsx
+++ packages/web/src/layout/Landing.tsx
@@ -2,23 +2,25 @@ import { Button } from "@/components/ui/button";
 
 export function Landing() {
   return (
-    <div className="max-w-[50rem] mx-auto pt-40 pb-4">
+    <div className="max-w-[60rem] mx-auto pt-40 pb-4 max-md:max-w-[90%]">
       <div className="w-full flex flex-col items-start justify-center">
-        <h1 className="text-[50pt] font-extrabold antialiased mb-[-0.3em] bg-gradient-to-b from-gray-100 to-gray-400 inline-block text-transparent bg-clip-text">
-          Innovating on the web
-        </h1>
-        <h2 className="text-[30pt] font-extrabold antialiased bg-gradient-to-b from-gray-300 to-gray-600 inline-block text-transparent bg-clip-text leading-none">
-          From concept to culmination
-        </h2>
-        <div className="w-full flex flex-col items-start gap-4 mt-4">
-          <p className="text-xl font-thin">
+        <div className="w-full flex flex-col items-start justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 bg-clip-text leading-none">
+          <h1 className="text-[5rem] font-extrabold antialiased mb-[-0.2em] inline-block text-transparent bg-clip-text leading-none">
+            Innovating on the web
+          </h1>
+          <h2 className="text-[4rem] font-extrabold antialiased inline-block text-transparent bg-clip-text leading-none">
+            From concept to culmination
+          </h2>
+        </div>
+        <div className="w-full flex flex-col items-start gap-4 mt-4 text-xl font-thin max-md:text-2xl">
+          <p>
             I'm a full stack developer with a passion for building scalable,
             performant, and secure web applications. Currently using
             technologies like Next.js, TypeScript, Postgresql, and tRPC to
             create innovative solutions that push the boundaries of modern web
             development.
           </p>
-          <p className="text-xl font-thin">
+          <p>
             With a plethora of other skills on standby, I'm always ready to take
             on new challenges and explore new frontiers in the world of web
             development.
