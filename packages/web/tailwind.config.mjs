5451992088d95aed62e57cc6ac44d012c9a368c3
zuice <jeff.allen127@gmail.com>
2024-08-30 23:03:48 -0400

    Add a better landing page, more to come

--- packages/web/tailwind.config.mjs
+++ packages/web/tailwind.config.mjs
@@ -72,6 +72,9 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      lineHeight: {
+        extratight: "0.6",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
